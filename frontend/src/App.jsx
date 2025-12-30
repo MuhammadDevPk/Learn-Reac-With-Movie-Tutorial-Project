59f9647b45f3f37e9ba06ddb24a67247bc6e8aa8
Malik Muhammad Awan <muhammad.dev.pk@gmail.com>
2025-12-30 18:59:45 +0500

    learned and implemented page routing and addded css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,21 @@
-import './App.css'
+import NavBar from './components/NavBar'
 import Favorites from './pages/Favorites'
 import Home from "./pages/Home"
 import { Routes, Route } from "react-router-dom"
+import "./css/App.css"
 
 function App() {
 
   return (
-    // <>
+    <div>
+      <NavBar />
     <main className="main-content">
      <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/favorites" element={<Favorites />} />
      </Routes>
      </main>
-    // </>
+     </div>
   )
 }
 
