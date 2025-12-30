bf9572f2aec38cafe4ca91684256617b17a3211e
Malik Muhammad Awan <muhammad.dev.pk@gmail.com>
2025-12-30 14:30:53 +0500

    clean up app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
 import './App.css'
-import MovieCard from "./components/MovieCard"
 
 function App() {
 
   return (
     <>
-      <MovieCard movie={{title: "Tim's Film", release_date: "2024"}}/>
-      <MovieCard movie={{title: "Joe's Film", release_date: "2020"}}/>
     </>
   )
 }
